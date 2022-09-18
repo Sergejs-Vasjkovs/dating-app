919486e568e3776c00f0b5a95279362e80d577fd
Sergejs-Vasjkovs <s.vasjkovs@gmail.com>
2022-09-18 23:17:43 +0300

    posttime-refactoring

--- src/app/utils/displayPostTime.js
+++ src/app/utils/displayPostTime.js
@@ -10,18 +10,25 @@ export const displayPostTime = (time) => {
     if (differens < (60 * 1000 * 10)) return "10 минут назад";
     if (differens < (60 * 1000 * 30)) return "30 минут назад";
     if (differens < (60 * 1000 * 60 * 24)) {
-        const hours = new Date(differens).getHours();
+        const hours = Math.floor(differens / 1000 / 60 / 60);
+        const min = Math.floor((differens / 1000 / 60 / 60 - hours) * 60);
         const hoursWord = pluralize(hours, "час", "часа", "часов");
-        const min = new Date(differens).getMinutes();
         const minWord = pluralize(min, "минута", "минуты", "минут");
-        return hours ? `${hours} ${hoursWord} : ${minWord} ${minWord} назад` : `${minWord} ${minWord} назад`;
+        return hours ? `${hours} ${hoursWord} и ${min} ${minWord} назад` : `${hours} ${hoursWord} назад`;
+    }
+    if (differens < (60 * 1000 * 60 * 24 * 30)) {
+        const day = Math.floor(differens / 1000 / 60 / 60 / 24);
+        const hours = Math.floor((differens / 1000 / 60 / 60 / 24 - day) * 24);
+        const dayhWord = pluralize(day, "день", "дня", "дней");
+        const hoursWord = pluralize(hours, "час", "часа", "часов");
+        return day ? `${day} ${dayhWord} ${hours ? `и ${hours} ${hoursWord}` : ``} назад` : `${hours} ${hoursWord} назад`;
     }
     if (differens < (60 * 1000 * 60 * 24 * 365)) {
-        const month = new Date(differens).getMonth();
+        const year = Math.floor(differens / 1000 / 60 / 60 / 24 / 365);
+        const month = Math.floor((differens / 1000 / 60 / 60 / 24 / 365 - year) * 12);
         const monthWord = pluralize(month, "месяц", "месяца", "месяцев");
-        const day = new Date(differens).getDay();
-        const dayWord = pluralize(day, "день", "дня", "дней");
-        return month ? `${month} ${monthWord} ${day ? `и ${day} ${dayWord}` : ``} назад` : `${day} ${dayWord} назад`;
+        const dayWord = pluralize(year, "день", "дня", "дней");
+        return month ? `${month} ${monthWord} ${year ? `и ${year} ${dayWord}` : ``} назад` : `${year} ${dayWord} назад`;
     }
     const year = new Date(differens).getFullYear();
     const yearWord = pluralize(year, "год", "года", "лет");
